6c6b70192d2936b13e56f0beec42129c54fbeddc
ilonaand <ilonaand@gmail.com>
2024-01-08 17:00:36 +0300

    fix(basic-3.4): Tag selectors are not used

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+var addButton=document.getElementById("add-button");//Add button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
@@ -33,17 +33,22 @@ var createNewTaskElement=function(taskString){
   var deleteButtonImg=document.createElement("img");//delete button image
 
   label.innerText=taskString;
-  label.className="task";
+  label.className="task-label list-item-label";
+
+  checkBox.className="list-item-checkbox";
+
+  listItem.className="list-item"
 
   //Each elements, needs appending
   checkBox.type="checkbox";
   editInput.type="text";
-  editInput.className="task";
+  editInput.className="task task-input task-input:focus";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.className="button-act edit";
 
-  deleteButton.className="delete";
+  deleteButton.className="button-act delete";
+  deleteButtonImg.className="delete-img";
   deleteButtonImg.src="./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
 
@@ -81,7 +86,7 @@ var editTask=function(){
 
   var listItem=this.parentNode;
 
-  var editInput=listItem.querySelector("input[type='text']");
+  var editInput=listItem.querySelector(".task-input");
   var label=listItem.querySelector("label");
   var editBtn=listItem.querySelector(".edit");
   var containsClass=listItem.classList.contains("edit-mode");
@@ -91,9 +96,13 @@ var editTask=function(){
       //switch to .editmode
       //label becomes the inputs value.
       label.innerText=editInput.value;
+      editInput.className="task task-input task-input:focus";
+      label.className="task-label list-item-label"
       editBtn.innerText="Edit";
   }else{
       editInput.value=label.innerText;
+      editInput.className="task task-input task-input:focus edit-mode-input"
+      label.className="task-label list-item-label edit-mode-label"
       editBtn.innerText="Save";
   }
 
